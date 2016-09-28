fcdcaf8388b29a30f49b4b263369c4acf13c145a
root <root@lxd-gui-app-01.lxd>
2016-09-28 14:12:54 +0000

    Tests fibers/exec

--- server/main.js
+++ server/main.js
@@ -2,4 +2,25 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  var Future = Npm.require("fibers/future");
+  var exec = Npm.require("child_process").exec;
+
+  Meteor.methods({
+    runCode: function () {
+      // This method call won't return immediately, it will wait for the
+      // asynchronous code to finish, so we call unblock to allow this client
+      // to queue other method calls (see Meteor docs)
+      this.unblock();
+      var future=new Future();
+      var command="curl -s -k --cert client.crt --key client.key https://192.168.1.79:8443/1.0 | jq .metadata.auth";
+      exec(command,function(error,stdout,stderr){
+        if(error){
+          console.log(error);
+          throw new Meteor.Error(500,command+" failed");
+        }
+        future.return(stdout.toString());
+      });
+      return future.wait();
+    }
+  });
 });
