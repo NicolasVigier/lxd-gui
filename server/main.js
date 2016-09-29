98929daf8966e1983a8eabb0af4ecc11cc7220d7
root <root@lxd-gui-app-01.lxd>
2016-09-29 08:38:34 +0000

    .

--- server/main.js
+++ server/main.js
@@ -13,13 +13,16 @@ Meteor.startup(() => {
       this.unblock();
       var future=new Future();
 // https://insights.ubuntu.com/2016/04/19/directly-interacting-with-the-lxd-api/
-      var command="curl -s -k --cert client.crt --key client.key https://192.168.1.79:8443/1.0/containers/lxd-gui-app-01/snapshots/ | jq .";
+	var target="/1.0";
+	console.log(target);
+      var command="curl -s -k --cert client.crt --key client.key https://192.168.1.79:8443"+ target + " | jq .";
       exec(command,function(error,stdout,stderr){
         if(error){
           console.log(error);
           throw new Meteor.Error(500,command+" failed");
         }
         future.return(stdout.toString());
+	console.log(stdout.toString());
       });
       return future.wait();
     }
