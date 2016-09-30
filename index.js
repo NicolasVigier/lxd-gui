b426fa5b4794ae7124030c9b74e5e43ef073b73e
NicolasVigier <nicolas.vigier@mycaelis.fr>
2016-09-30 08:41:43 +0200

    - sessions

--- index.js
+++ index.js
@@ -24,10 +24,7 @@
 //  },
 //});
 
-import { Session } from 'meteor/session';
 
   Meteor.call('runCode', function (err, response) {
     console.log(response);
   });
-
-  Session.setDefault("container", "my-container");
